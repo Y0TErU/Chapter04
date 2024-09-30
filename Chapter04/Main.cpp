254b8216c8cf0c638b190e6c8cecd06cd048e6c5
warusurayooteru <warusura.yooteru@gmail.com>
2024-09-30 16:32:47 +0900

    04-03

--- Chapter04/Main.cpp
+++ Chapter04/Main.cpp
@@ -2,13 +2,24 @@
 using namespace std;
 
 //変数の前に & をつけることでその変数のアドレスを取得できる
+//アドレスに　*　をつけるとポインタを作成できる
+
 int main()
 {
-	char a;
-	char* p;		//ポインタ
+	int a;
+	int* p = &a;		//ポインタ
+	/*
+		変数 a のアドレスを変数 p に代入する
+		a のアドレスを参照することが出来るようになる
 
-	p = &a;
-	cout << "p = " << (size_t)p << endl
-		<< "&a = " << (size_t)a << endl;
+	*/
+
+	a = 0;
+	cout << "a = " << a << endl
+		<< "*p = " << *p << endl;
+	
+	*p = 5;
+	cout << "a = " << a << endl
+		<< "*p = " << *p << endl;
 
 }
