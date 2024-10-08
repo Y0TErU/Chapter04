e2a7a1b653bd678e8416a730dadac156f86af8cc
warusurayooteru <warusura.yooteru@gmail.com>
2024-10-08 13:32:56 +0900

    04-06

--- Chapter04/Main.cpp
+++ Chapter04/Main.cpp
@@ -2,25 +2,12 @@
 using namespace std;
 
 //変数の前に & をつけることでその変数のアドレスを取得できる
-void ShowAddrsses(char* pointer)
-{
-	cout << "pointer   : " << (size_t)pointer << endl;
-	for (int i = 0; i < 10; i++)
-	{
-		cout << "&pointer[ " << i << " ]   : " << (size_t)&pointer[i] << endl;
-	}
-}
-
 int main()
 {
-	char array[10];
-	cout << "arraay   : " << (size_t)array << endl;				//変数arrayのアドレスを表示
-	for (int i = 0; i < 10; i++)
-	{
-		cout << "&array[" << i << " ]  : " << (size_t) & array[i] << endl;	//変数arrayのそれぞれの配列のアドレスを表示
-	}
-	cout << endl;
+	int n[5];
 
-	ShowAddrsses(array);
+	cout << "&n[2]   = " << (size_t)&n[2] << endl
+		<< "&n[2] + 1 = " << (size_t)(&n[2] + 1) << endl
+		<< "&n[3]   = " << (size_t)&n[3] << endl;
 
 }
