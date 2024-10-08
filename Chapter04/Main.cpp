cdb06694e6a12fd23d26fc5183008f99cc5f79d5
warusurayooteru <warusura.yooteru@gmail.com>
2024-10-08 14:07:58 +0900

    04-06-2

--- Chapter04/Main.cpp
+++ Chapter04/Main.cpp
@@ -4,10 +4,14 @@ using namespace std;
 //変数の前に & をつけることでその変数のアドレスを取得できる
 int main()
 {
-	int n[5];
+	int n[4];
+	int* p = n;
 
-	cout << "&n[2]   = " << (size_t)&n[2] << endl
-		<< "&n[2] + 1 = " << (size_t)(&n[2] + 1) << endl
-		<< "&n[3]   = " << (size_t)&n[3] << endl;
+	cout << "p     : " << (size_t)p << endl
+		<< "&n[0] : " << (size_t)&n[0] << endl;
+
+	p += 2;
+	cout << "p     : " << (size_t)p << endl
+		<< "&n[2] : " << (size_t)&n[2] << endl;
 
 }
