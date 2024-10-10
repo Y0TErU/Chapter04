bfb2fe44f6a4a04d5d4f332fa0647fddf3aebf6a
warusurayooteru <warusura.yooteru@gmail.com>
2024-10-10 14:43:15 +0900

    04-09

--- Chapter04/Main.cpp
+++ Chapter04/Main.cpp
@@ -1,29 +1,27 @@
 #include <iostream>
 using namespace std;
 
-//変数の前に & をつけることでその変数のアドレスを取得できる
+const int ARRAY_SIZE = 5;
 void Init(int array[])
 {
 	for (int i = 0; i < 5; i++)
 	{
 		array[i] = i * 5;
 	}
-	
 }
 
 void Show(int array[])
 {
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < ARRAY_SIZE; i++)
 	{
 		cout << array[i] << ' ';
 	}
 	cout << endl;
-
 }
 
 int main()
 {
-	int n[5];
+	int n[ARRAY_SIZE];
 
 	Init(n);
 	Show(n);
